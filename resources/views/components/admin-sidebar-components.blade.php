5abbd1fb7302156a80cade5acad48313b0dc1e75
Tahmid <taahmid15-6966@diu.edu.bd>
2021-03-09 15:05:27 +0600

    search not working on main page fixed

--- resources/views/components/admin-sidebar-components.blade.php
+++ resources/views/components/admin-sidebar-components.blade.php
@@ -111,4 +111,21 @@
 </li>
 <!-- Divider -->
 
+<hr class="sidebar-divider">
+
+<!-- Nav Item - Pages Collapse Menu (Category)-->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSettings" aria-expanded="true" aria-controls="collapseSettings">
+        <i class="fas fa-fw fa-cog"></i>
+        <span>Settings</span>
+    </a>
+    <div id="collapseSettings" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Category</h6>
+            <a class="collapse-item" href="{{ route('security.index') }}">Update Password</a>
+        </div>
+    </div>
+</li>
+<!-- Divider -->
+
 
--- resources/views/pages/sitePage/main-page.blade.php
+++ resources/views/pages/sitePage/main-page.blade.php
@@ -111,8 +111,9 @@
 
 
 
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <form class="form-inline my-2 my-lg-0" action="{{ route('search.store') }}" method="post">
+                {{ csrf_field() }}
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
 
